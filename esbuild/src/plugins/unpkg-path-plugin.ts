74247a58a55f1484d736194a75ac17956fed1024
pranjal <pranjalwalia77@gmail.com>
2021-03-19 00:23:21 +0530

    path resolution algo complete

--- esbuild/src/plugins/unpkg-path-plugin.ts
+++ esbuild/src/plugins/unpkg-path-plugin.ts
@@ -1,15 +1,28 @@
 import * as esbuild from "esbuild-wasm";
+import axios from "axios";
 
-export const unpkgPathPlugin = (): {
-  name: string;
-  setup(build: esbuild.PluginBuild): void;
-} => {
+export const unpkgPathPlugin = () => {
   return {
     name: "unpkg-path-plugin",
     setup(build: esbuild.PluginBuild) {
       build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log("onResole", args);
-        return { path: args.path, namespace: "a" };
+        console.log("onResolve", args);
+        if (args.path === "index.js") {
+          return { path: args.path, namespace: "a" };
+        }
+
+        if (args.path.includes("./") || args.path.includes("../")) {
+          return {
+            namespace: "a",
+            path: new URL(args.path, `https://unpkg.com${args.resolveDir}/`)
+              .href,
+          };
+        }
+
+        return {
+          namespace: "a",
+          path: `https://unpkg.com/${args.path}`,
+        };
       });
 
       build.onLoad({ filter: /.*/ }, async (args: any) => {
@@ -19,16 +32,18 @@ export const unpkgPathPlugin = (): {
           return {
             loader: "jsx",
             contents: `
-              import message from './message';
+              const message = require('nested-test-pkg');
               console.log(message);
             `,
           };
-        } else {
-          return {
-            loader: "jsx",
-            contents: 'export default "hi there!"',
-          };
         }
+
+        const { data, request } = await axios.get(args.path);
+        return {
+          loader: "jsx",
+          contents: data,
+          resolveDir: new URL("./", request.responseURL).pathname,
+        };
       });
     },
   };
