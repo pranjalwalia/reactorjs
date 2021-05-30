065e6dbe135d29099883145e8a9c0c21c74bef97
pranjal <pranjalwalia77@gmail.com>
2021-05-30 16:34:54 +0530

    refactor: removed promise based engine execution
    - applied async/await to refactor earlier promise based execution of
    build engine

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -8,45 +8,49 @@ import { iFrameIdentifier } from '../utils/sandboxNameGenerator';
 
 export const App: React.FC<{}> = (): ReactElement | null => {
     const [inputCode, setInputCode] = useState<string>('');
+    const [transpiledCode, setTranspiledCode] = useState<string>('');
     const [bundledCode, setBundledCode] = useState<string | null>('');
     const ref = useRef<any>();
+    // const frameRef = useRef<any>();
 
     useEffect((): void => {
         initializeService();
         ref.current = true;
     }, []);
 
-    const bundlerInitialize = (e: React.MouseEvent<HTMLElement>): void => {
+    const bundlerInitialize = async (e: React.MouseEvent<HTMLElement>): Promise<void> => {
         if (!ref.current) {
             return;
         }
 
-        transpile(inputCode, { loader: 'jsx', target: 'es2015' })
-            .then(({ code }) => {
-                console.log('Successfully Transpiled cell');
-                setBundledCode(code);
-            })
-            .catch((err) => {
-                console.log(`Transpilation failed with error: ${err.message}`);
-                return;
+        try {
+            const { code, warnings } = await transpile(inputCode, {
+                loader: 'jsx',
+                target: 'es2015'
             });
+            console.log('Successfully Transpiled cell');
+            setTranspiledCode(code);
+        } catch (err) {
+            console.log(`Transpilation failed with error: ${err.message}`);
+            return;
+        }
 
-        buildSystem({
-            entryPoints: ['index.js'],
-            bundle: true,
-            write: false,
-            plugins: [unpkgBypassPathPlugin(), unpkgBypassFetchPlugin(inputCode)],
-            define: {
-                global: 'window',
-                'process.env.NODE_ENV': '"production"'
-            }
-        })
-            // bundled output
-            .then(({ outputFiles }) =>
-                setBundledCode(outputFiles !== null ? outputFiles![0].text : null)
-            )
+        try {
+            const { outputFiles, warnings } = await buildSystem({
+                entryPoints: ['index.js'],
+                bundle: true,
+                write: false,
+                plugins: [unpkgBypassPathPlugin(), unpkgBypassFetchPlugin(inputCode)],
+                define: {
+                    global: 'window',
+                    'process.env.NODE_ENV': '"production"'
+                }
+            });
+            setBundledCode(outputFiles !== null ? outputFiles![0].text : null);
+        } catch (err) {
             // outputFiles is undefined
-            .catch((err) => setBundledCode(err.message));
+            setBundledCode(err.message);
+        }
     };
 
     const iFramePayload: string = `<script> ${bundledCode} </script>`;
