4b8aacacafa904af0df4ba06fb41fcfd51d46379
pranjal <pranjalwalia77@gmail.com>
2021-05-30 16:02:18 +0530

    feat: `iframe` bundler code injection
    - code injected into the `iframe` via `srcDoc`
    - bug: fails on bundles that may have `script` tags, pending fix

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,10 @@
 import React, { ReactElement, useState, useEffect, useRef } from 'react';
+
 import { initializeService, transpile, buildSystem } from '../services/engine/buildEngine';
 import { unpkgBypassPathPlugin } from '../plugins/unkpg-bypass-path-plugin';
 import { unpkgBypassFetchPlugin } from '../plugins/unpkg-bypass-fetch-plugin';
+
+import { iFrameIdentifier } from '../utils/sandboxNameGenerator';
 
 export const App: React.FC<{}> = (): ReactElement | null => {
     const [inputCode, setInputCode] = useState<string>('');
@@ -13,7 +16,7 @@ export const App: React.FC<{}> = (): ReactElement | null => {
         ref.current = true;
     }, []);
 
-    const startBundling = (e: React.MouseEvent<HTMLElement>): void => {
+    const bundlerInitialize = (e: React.MouseEvent<HTMLElement>): void => {
         if (!ref.current) {
             return;
         }
@@ -39,18 +42,28 @@ export const App: React.FC<{}> = (): ReactElement | null => {
             }
         })
             // bundled output
-            .then(({ outputFiles }) => setBundledCode(outputFiles![0].text))
+            .then(({ outputFiles }) =>
+                setBundledCode(outputFiles !== null ? outputFiles![0].text : null)
+            )
             // outputFiles is undefined
             .catch((err) => setBundledCode(err.message));
     };
+
+    const iFramePayload: string = `<script> ${bundledCode} </script>`;
+    const iFrameTitle: string = `iframe-${iFrameIdentifier(iFramePayload.length % 123)}`;
 
     return (
         <div>
             <textarea value={inputCode} onChange={(e) => setInputCode(e.target.value)}></textarea>
             <div>
-                <button onClick={startBundling}>Submit</button>
+                <button onClick={bundlerInitialize}>Submit</button>
             </div>
             <pre>{bundledCode}</pre>
+            <iframe
+                src="/test.html"
+                title={iFrameTitle}
+                sandbox="allow-scripts"
+                srcDoc={iFramePayload}></iframe>
         </div>
     );
 };
