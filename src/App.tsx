27fcc9b75c3fe320c624ed94ff959f7d090642df
pranjal <pranjalwalia77@gmail.com>
2021-05-31 18:54:51 +0530

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
-import { Counter } from './Counter'
-import './App.css'
-
 export const App = () => {
   return (
     <>
       <h1>React TypeScript Webpack Starter Template</h1>
-      <Counter />
     </>
   )
 }
