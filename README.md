0172c998b9d2e591a39d00740e89d008f47efde9
pranjal <pranjalwalia77@gmail.com>
2021-06-05 20:41:58 +0530

    cleanup

--- README.md
+++
@@ -1,5 +0,0 @@
-# Boiler plate with webpack merge
-
-## setup with babel and typescript loaders
-
-## out of the box support for jest
--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-export const App = () => {
-  return (
-    <div>
-      <h1>React TypeScript Webpack Starter Template</h1>
-    </div>
-  )
-}
