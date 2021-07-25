87106c9dba725baf167e776a9a8bad59cd792db1
Pranjal Walia <pranjalwalia77@gmail.com>
2021-07-25 18:17:18 +0530

    feat: cleanup and start iframe splitting
    - cleanup after refactor build engine api
    - pending: iframe ref resolution

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,5 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import React, { ReactElement, useState, useEffect, useRef } from 'react';
-
 import { initializeService, engineGenerateBundledCode } from '../services/engine/buildEngine';
 import { iFrameIdentifier } from '../utils/sandboxNameGenerator';
 
@@ -9,7 +8,6 @@ import Editor from '../containers/CodeEditor';
 
 export const App: React.FC<{}> = (): ReactElement | null => {
     const [inputCode, setInputCode] = useState<string>('');
-    // const [transpiledCode, setTranspiledCode] = useState<string>('');
     const [bundledCode, setBundledCode] = useState<string | null>('');
 
     const iFrameRef = useRef<any>();
@@ -22,38 +20,10 @@ export const App: React.FC<{}> = (): ReactElement | null => {
         if (!inputCode) {
             return;
         }
-        // try {
-        //     const { code } = await transpile(inputCode, {
-        //         loader: 'jsx',
-        //         target: 'es2015'
-        //     });
-        //     console.log('Successfully Transpiled cell');
-        //     setTranspiledCode(code);
-        // } catch (err) {
-        //     console.log(`Transpilation failed with error: ${err.message}`);
-        //     return;
-        // }
-
-        // try {
-        //     const { outputFiles } = await buildSystem({
-        //         entryPoints: ['index.js'],
-        //         bundle: true,
-        //         write: false,
-        //         plugins: [unpkgBypassPathPlugin(), unpkgBypassFetchPlugin(inputCode)],
-        //         define: {
-        //             global: 'window',
-        //             'process.env.NODE_ENV': '"production"'
-        //         }
-        //     });
         const res = await engineGenerateBundledCode(inputCode);
         setBundledCode(res);
         iFrameRef.current.contentWindow.postMessage(res, '*');
     };
-    // catch (err) {
-    //     // outputFiles is undefined
-    //     console.log(err.message);
-    // }
-    // };
 
     const iFramePayload: string = `
     <html>
@@ -89,16 +59,9 @@ export const App: React.FC<{}> = (): ReactElement | null => {
                     executableCodeOnChangeHandler={(value: string) => setInputCode(value)}
                 />
             </div>
-            {/* <textarea
-                ref={inputCodeRef}
-                value={inputCode}
-                onChange={(e) => {
-                    setInputCode(e.target.value);
-                }}></textarea> */}
             <div>
                 <button onClick={bundlerInitialize}>Submit</button>
             </div>
-            {/* <pre>{bundledCode}</pre> */}
             <div>
                 {/* {bundledCode ? <Preview bundledCode={bundledCode} /> : null} */}
                 <iframe
@@ -108,6 +71,7 @@ export const App: React.FC<{}> = (): ReactElement | null => {
                     sandbox="allow-scripts"
                     srcDoc={iFramePayload}></iframe>
             </div>
+            <pre>{bundledCode}</pre>
         </div>
     );
 };
